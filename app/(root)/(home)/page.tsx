001a4b614947712a038f5611b232e672718879cc
im-Kazmi <abidkazmi729@gmail.com>
2023-11-10 06:57:22 -1100

    adding clerk auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="bg-red-300 justify-center flex text-red-500 ">
-      <div>hi</div>
+      <UserButton />
     </main>
   );
 }
