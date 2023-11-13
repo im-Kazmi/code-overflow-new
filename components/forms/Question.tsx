1c82494bef7ad94441f9ec98340c3dadd43b3d98
im-Kazmi <abidkazmi729@gmail.com>
2023-11-13 04:20:03 -1100

    completing add-question page

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import { useForm } from "react-hook-form";
+import { FieldElement, FieldName, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { questionSchema } from "@/lib/validations";
 import { z } from "zod";
@@ -29,7 +29,40 @@ const Question = () => {
     },
   });
 
+  const { isSubmitting, isLoading, isDirty } = form.formState;
+
   function onSubmit(values: z.infer<typeof questionSchema>) {}
+
+  const handleTagDelete = (selectedTag: any) => {
+    const tags = form.getValues("tags").filter((tag) => tag !== selectedTag);
+    form.setValue("tags", tags);
+  };
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            message: "Tag must be less than 15 characters",
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
   return (
     <Form {...form}>
       <form
@@ -68,23 +101,46 @@ const Question = () => {
               </FormLabel>
               <FormControl>
                 <Editor
-                  onInit={(evt, editor) => (editorRef.current = editor)}
-                  initialValue="<p>This is the initial content of the editor.</p>"
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
                   init={{
                     height: 500,
                     menubar: false,
                     plugins: [
-                      "advlist autolink lists link image charmap print preview anchor",
-                      "searchreplace visualblocks code fullscreen",
-                      "insertdatetime media table paste code help wordcount",
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "print",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "code",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "paste",
+                      "code",
+                      "help",
+                      "wordcount",
+                      "preview",
                     ],
                     toolbar:
                       "undo redo | formatselect | " +
-                      "bold italic backcolor | alignleft aligncenter " +
+                      "codesample bold italic backcolor | alignleft aligncenter " +
                       "alignright alignjustify | bullist numlist outdent indent | " +
-                      "removeformat | help",
+                      "removeformat | help preview ",
                     content_style:
-                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                      "body { font-family:Inter,sans-serif; font-size:16px; background-color:rgb(20,20,20);color:white; }",
                   }}
                 />
               </FormControl>
@@ -105,13 +161,29 @@ const Question = () => {
                 <Input
                   placeholder="Add Tags"
                   className=" bg-black/40 text-white border-none outline-none focus:ring-0 ring-0"
-                  {...field}
+                  onKeyDown={(e) => handleInputKeyDown(e, field)}
                 />
+                {/* {field.value.length > 0 && ( */}
+
+                {/* )} */}
               </FormControl>
-              <FormDescription>
-                Add upto 3 tags and describe what your questions is about. press
-                Enter to add a Tag.
-              </FormDescription>
+              <FormDescription></FormDescription>
+              <div className=" flex w-full gap-3 mt-5">
+                {field.value.map((tag) => (
+                  <div
+                    key={tag}
+                    className=" w-fit px-3 cursor-pointer gap-5 py-1 rounded-lg bg-orange-100 text-white bg-opacity-20"
+                  >
+                    {tag}
+                    <span
+                      onClick={() => handleTagDelete(tag)}
+                      className="ml-5 text-white"
+                    >
+                      x
+                    </span>
+                  </div>
+                ))}
+              </div>
               <FormMessage className=" text-red-600" />
             </FormItem>
           )}
@@ -120,7 +192,7 @@ const Question = () => {
           type="submit"
           className=" bg-gradient-to-r from-orange-500 to-orange-200"
         >
-          Submit
+          {isSubmitting ? "Submitting..." : "Submit"}
         </Button>
       </form>
     </Form>
