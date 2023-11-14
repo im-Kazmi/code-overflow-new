e1fea6f73a117ebc0d1ea49c2d41f0a5049b6363
im-Kazmi <abidkazmi729@gmail.com>
2023-11-14 19:20:28 +0500

    removing error from all files

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,32 +1,31 @@
+/* eslint-disable no-unused-vars */
 "use client";
 import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
   mode: string;
-  setMode: () => void;
+  // setMode: () => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState("dark");
-  const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("bg-white");
-    } else {
-      setMode("dark");
-      document.documentElement.classList.add("bg-black/80");
-    }
-  };
+  // const handleThemeChange = () => {
+  //   if (mode === "dark") {
+  //     setMode("light");
+  //     document.documentElement.classList.add("bg-white");
+  //   } else {
+  //     setMode("dark");
+  //     document.documentElement.classList.add("bg-black/80");
+  //   }
+  // };
 
   // useEffect(() => {
   //   handleThemeChange();
   // }, [mode]);
 
   return (
-    <ThemeContext.Provider value={{ mode, handleThemeChange }}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={{ mode }}>{children}</ThemeContext.Provider>
   );
 };
 
